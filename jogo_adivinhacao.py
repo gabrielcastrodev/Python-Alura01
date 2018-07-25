a5f6dc0e883d8f5dd30da7388e0be33c9621a6a1
gabrielcastrodev <gcastrop5@gmail.com>
2018-07-25 14:57:35 -0300

    Commit for

--- jogo_adivinhacao.py
+++ jogo_adivinhacao.py
@@ -8,12 +8,17 @@ total_tentativas = 3
 
 for rodada in range (1, total_tentativas + 1):
     print("Tentativa, {} de {}".format(rodada, total_tentativas))
-    chute = int(input("Digite o seu número: "))
+    chute = int(input("Digite um número entre 1 e 100: "))
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
     menor = chute < numero_secreto
 
     print("Você digitou ", chute)
+
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        print()
+        continue
 
     if(acertou):
         print("Você acertou!")
