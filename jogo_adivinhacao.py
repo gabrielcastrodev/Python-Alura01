65728ae4d69ae96e59fdb612672d3f13d75a57b5
gabrielcastrodev <gcastrop5@gmail.com>
2018-07-25 01:06:13 -0300

    While

--- jogo_adivinhacao.py
+++ jogo_adivinhacao.py
@@ -1,24 +1,26 @@
 print("*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
-print();
+print()
 
 numero_secreto = 42
+total_tentativas = 3
 
-chute = int(input("Digite o seu número: "))
+while(total_tentativas > 0):
+    chute = int(input("Digite o seu número: "))
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-acertou = chute == numero_secreto
-maior = chute > numero_secreto
-menor = chute < numero_secreto
+    print("Você digitou ", chute)
 
-print("Você digitou ", chute)
-
-if(acertou):
-    print("Você acertou!")
-else:
-    if (maior):
-        print("Seu chute foi maior do que o número secreto!")
-    elif (menor):
-        print("Seu chuto foi menor do que o número secreto!")
+    if(acertou):
+        print("Você acertou!")
+    else:
+        if (maior):
+            print("Seu chute foi maior do que o número secreto!")
+        elif (menor):
+            print("Seu chute foi menor do que o número secreto!")
+    total_tentativas = total_tentativas -1
 
 print("Fim de jogo!")
